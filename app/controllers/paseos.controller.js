31ffd6dee5308fc9218665650dfc817b1be2e94a
Emanuel Gómez Díaz <egomez413@misena.edu.co>
2023-06-22 01:27:24 -0500

    CRU DE PASEOS
    Pendiente de hacer el delete de paseos, de resto todas funcionan, tambien pendiente acabar la api de perros, usuarios, chat y unir api con front

--- app/controllers/paseos.controller.js
+++ app/controllers/paseos.controller.js
@@ -60,24 +60,29 @@ export const postPaseo = async (req, res) => {
 //Traer todos los paseos de la base de datos
 export const getTodosPaseos = async (req, res) => {
     try {
-        const dog = {id: req.params.id}
-        // Declarar colección
-        const result = db.collection('usuario').doc(dog.id).get();
-        result.then((querySnapshot) => {
-            const data = [];
-            querySnapshot.forEach((doc) => {
-                data.push(doc.data());
-            });
-            let jsonData = JSON.stringify(data);
-            res.json(JSON.parse(jsonData));
-        })
+        const paseoId = req.params.id;
+        
+        // Obtener la colección de paseos
+        const paseosRef = db.collection('paseo');
+        
+        // Obtener todos los paseos
+        const querySnapshot = await paseosRef.get();
+        
+        const data = [];
+        
+        // Recorrer los documentos y agregar los datos a la matriz 'data'
+        querySnapshot.forEach((doc) => {
+            data.push(doc.data());
+        });
+        
+        res.json(data);
     } catch (error) {
         message(error.message, "danger");
         res.status(500);
     }
 }
 
-//Traer un paseo de la base de datos
+//Traer un paseo especifico de la base de datos
 export const getPaseo = async (req, res) => {
     try {
         // Declarar datos del usuario
@@ -106,17 +111,45 @@ export const getPaseo = async (req, res) => {
 //Modifica un paseo 
 export const updatePaseo = async (req, res) => {
     try {
-        const dog = {id: req.params.id}
+        // Declarar datos del usuario
+        const paseo = {
+            "descripcion": req.body.descripcionPaseo,
+            "destino": {
+                "_latitude":req.body.paseoLatitude,
+                "_longitude": req.body.paseoLongitude,
+            },
+            "dueno": {
+                "id_dueno":req.body.duenoIdPaseo,
+                "img_dueno":req.body.duenoImgPaseo,
+                "nombre_dueno":req.body.duenoNombrePaseo,
+            },
+            "estado": req.body.estadoPaseo,
+            "hora_fin": req.body.horaFinPaseo,
+            "hora_inicio": req.body.horaInicioPaseo,
+            "medio_de_pago": req.body.medioPagoPaseo,
+            "nombre_destino": req.body.nombreDestinoPaseo,
+            "paseador": {
+                "id_paseador":req.body.paseadorIdPaseo,
+                "img_paseador":req.body.paseadorImgPaseo,
+                "nombre_paseador":req.body.paseadorNombrePaseo,
+            },
+            "perro": {
+                "id_perro":req.body.perroIdPaseo,
+                "img_perro":req.body.perroImgPaseo,
+                "localizacion":req.body.perroLocalizacionPaseo,
+                "nombre_perro":req.body.perroNombrePaseo,
+            },
+            "precio": req.body.precioPaseo
+        }
+
         // Declarar colección
-        const result = db.collection('usuario').doc(dog.id).get();
-        result.then((querySnapshot) => {
-            const data = [];
-            querySnapshot.forEach((doc) => {
-                data.push(doc.data());
-            });
-            let jsonData = JSON.stringify(data);
-            res.json(JSON.parse(jsonData));
-        })
+        const paseoRef  = db.collection('paseo');
+
+        // Declarar documento y actualizar los campos con los datos del usuario
+        const result = await paseoRef.doc(req.params.id).update(paseo);
+
+        res.json(result);
+        message("SI SE PUDO", "success");
     } catch (error) {
         message(error.message, "danger");
         res.status(500);
--- app/routes/paseos.routes.js
+++ app/routes/paseos.routes.js
@@ -4,16 +4,26 @@ import * as userController from "../controllers/paseos.controller";
 const router = Router();
 
 // C POST
+
+// CREAR UN PASEO 
 router.post('/paseo/', /*userController.isValidToken,*/ userController.postPaseo);
 
 // R GET
-router.get('/paseo/', /*userController.isValidToken,*/ userController.getTodosPaseos);
+
+// TRAER TODOS LOS PASEOS
+router.get('/paseo', /*userController.isValidToken,*/ userController.getTodosPaseos);
+
+// TRAER UN PASEO ESPECIFICO
 router.get('/paseo/:id', /*userController.isValidToken,*/ userController.getPaseo);
 
 // U PUT
+
+// MODIFICAR UN PASEO
 router.put('/paseo/:id', /*userController.isValidToken,*/ userController.updatePaseo);
 
-// D Delete
+// D DELETE
+
+// BORRAR UN PASEO
 router.delete('/paseo/:id', /*userController.isValidToken,*/ userController.deletePaseo);
 
 export default router;
